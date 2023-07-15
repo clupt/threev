85dd7aaf27746af5b791850a4f857168d546ed6e
Chris Lupton <42554017+clupt@users.noreply.github.com>
2023-07-15 11:48:40 -0500

    new vite threejs scratchpad

---
+++ main.js
@@ -0,0 +1,36 @@
+import './style.css'
+import * as THREE from 'three';
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+const axesHelper = new THREE.AxesHelper(5);
+scene.add(axesHelper);
+
+//camera.position.set(5, 2, 3);
+camera.position.x = 0;
+camera.position.y = 2;
+camera.position.z = 5;
+
+const boxGeometry = new THREE.BoxGeometry();
+const boxMaterial = new THREE.MeshBasicMaterial({color: 0x00FF00});
+const box = new THREE.Mesh(boxGeometry, boxMaterial);
+scene.add(box);
+
+function animate(time) {
+  box.rotation.x = time / 1000;
+  box.rotation.y = time / 1000;
+  renderer.render(scene, camera);
+}
+
+renderer.setAnimationLoop(animate);
