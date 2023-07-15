521e3070f5294c4627cfd4e4c4ca81539386c908
Chris Lupton <42554017+clupt@users.noreply.github.com>
2023-07-15 12:06:15 -0500

    Add orbit controls and set plane geometry

--- main.js
+++ main.js
@@ -1,31 +1,63 @@
-import './style.css'
+import './style.css';
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
+//renderer
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
+//scene
 const scene = new THREE.Scene();
 
+//camera
 const camera = new THREE.PerspectiveCamera(
-  75,
+  45,
   window.innerWidth / window.innerHeight,
   0.1,
   1000
 );
 
+const orbit = new OrbitControls(camera, renderer.domElement);
+orbit.update(); //call update method every time camera position changes
+
+
 const axesHelper = new THREE.AxesHelper(5);
 scene.add(axesHelper);
 
-//camera.position.set(5, 2, 3);
-camera.position.x = 0;
-camera.position.y = 2;
-camera.position.z = 5;
+camera.position.set(-10, 30, 30);
+// camera.position.x = 0;
+// camera.position.y = 2;
+// camera.position.z = 5;
 
+//object
 const boxGeometry = new THREE.BoxGeometry();
-const boxMaterial = new THREE.MeshBasicMaterial({color: 0x00FF00});
+const boxMaterial = new THREE.MeshBasicMaterial(
+  {
+    color: 0x00FF00,
+    wireframe: true
+  }
+);
+
 const box = new THREE.Mesh(boxGeometry, boxMaterial);
 scene.add(box);
+
+//plane
+const planeGeometry = new THREE.PlaneGeometry(30, 30);
+const planeMaterial = new THREE.MeshBasicMaterial(
+  {
+    color: 0xFFFFFF,
+    side: THREE.DoubleSide
+  }
+);
+const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+scene.add(plane);
+plane.rotation.x = -0.5 * Math.PI; //make plane match grid
+
+const gridHelper = new THREE.GridHelper(30);
+scene.add(gridHelper);
+
+
 
 function animate(time) {
   box.rotation.x = time / 1000;
