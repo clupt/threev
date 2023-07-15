4447413d382e35bfc03a767f7e8a0b26b11eba68
Chris Lupton <42554017+clupt@users.noreply.github.com>
2023-07-15 13:02:35 -0500

    add basic textures for objs

--- main.js
+++ main.js
@@ -27,25 +27,34 @@ scene.add(axesHelper);
 
 camera.position.set(-10, 30, 30);
 
+//textures
+const textureLoader = new THREE.TextureLoader();
+const plantTexture = textureLoader.load(
+  'plants.jpg'
+);
+const woodTexture = textureLoader.load(
+  'wood.jpg'
+)
+
 //objects
 const boxGeometry = new THREE.BoxGeometry();
 const boxMaterial = new THREE.MeshBasicMaterial(
   {
-    color: 0x00FF00,
-    wireframe: true
+    color: 0x00ff00,
+    wireframe: false,
+    map: plantTexture
   }
 );
 
 const box = new THREE.Mesh(boxGeometry, boxMaterial);
 scene.add(box);
 
-
 const sphereGeometry = new THREE.SphereGeometry(4, 50, 50);//radius, w, h
-
 const sphereMaterial = new THREE.MeshStandardMaterial(
   {
-    color: 0x0000FF,
-    wireframe: false
+    color: 0xffffff,
+    wireframe: false,
+    map: woodTexture
   }
 );
 
@@ -53,11 +62,12 @@ const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
 scene.add(sphere);
 sphere.position.set(-10, 10, 0);
 
+
 //lights
 const pointLight = new THREE.PointLight(0xffffff);
-pointLight.position.set(10, 5, 5);
+pointLight.position.set(5, 5, 5);
 
-const ambientLight = new THREE.AmbientLight(0xffffff);
+const ambientLight = new THREE.AmbientLight(0x404040);//soft white light
 scene.add(pointLight, ambientLight);
 
 const lightHelper = new THREE.PointLightHelper(pointLight);
@@ -67,7 +77,7 @@ scene.add(lightHelper);
 const planeGeometry = new THREE.PlaneGeometry(30, 30);
 const planeMaterial = new THREE.MeshBasicMaterial(
   {
-    color: 0xffffff,
+    color: 0xaaaaff,
     side: THREE.DoubleSide
   }
 );
@@ -83,6 +93,10 @@ scene.add(gridHelper);
 function animate(time) {
   box.rotation.x = time / 1000;
   box.rotation.y = time / 1000;
+  sphere.rotation.x = time / 1000;
+  sphere.rotation.y = time / 1000;
+  // pointLight.position.set(Math.random(), 5, 5); //attempt at light flickering
+
   renderer.render(scene, camera);
 }
 
