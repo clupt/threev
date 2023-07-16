c7079929d3cfcb1b36e0de388f202ee3f9b0614a
Chris Lupton <42554017+clupt@users.noreply.github.com>
2023-07-16 12:26:11 -0500

    organize sections and add spotlight controls

--- main.js
+++ main.js
@@ -4,15 +4,18 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import * as dat from 'dat.gui';
 
 
-//renderer
+/****************************** RENDERER **************************************/
 const renderer = new THREE.WebGLRenderer();
+
+renderer.shadowMap.enabled = true;
+
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 //scene
 const scene = new THREE.Scene();
 
-//camera
+/****************************** CAMERA ****************************************/
 const camera = new THREE.PerspectiveCamera(
   45,
   window.innerWidth / window.innerHeight,
@@ -23,13 +26,9 @@ const camera = new THREE.PerspectiveCamera(
 const orbit = new OrbitControls(camera, renderer.domElement);
 orbit.update(); //call update method every time camera position changes
 
-
-const axesHelper = new THREE.AxesHelper(5);
-scene.add(axesHelper);
-
 camera.position.set(-10, 30, 30);
 
-//gui
+/******************************** GUI *****************************************/
 const gui = new dat.GUI();
 
 const options = {
@@ -37,12 +36,25 @@ const options = {
   sphereWireframe: false,
   // torusWireframe: false,
   sphereBounceSpeed: 0.01,
+  angle: 0.2,
+  penumbra: 0,
+  intensity: 1,
+  decay: 0,
+  spotLightColor: '#f5f582'
 }
 
 gui.add(options, 'sphereBounceSpeed', 0, .2); // min/max (set in animate fn)
+gui.add(options, 'angle', 0, 1);
+gui.add(options, 'penumbra', 0, 1);
+gui.add(options, 'intensity', 0, 1);
+gui.add(options, 'decay', 0, 2);
 
 gui.addColor(options, 'sphereColor').onChange((evt)=>{
   sphere.material.color.set(evt)
+});
+
+gui.addColor(options, 'spotLightColor').onChange((evt)=>{
+  spotLight.color.set(evt)
 });
 
 gui.add(options, 'sphereWireframe').onChange((evt)=>{
@@ -53,7 +65,7 @@ gui.add(options, 'sphereWireframe').onChange((evt)=>{
 //   torus.material.wireframe = evt
 // });
 
-//textures
+/****************************** TEXTURES **************************************/
 const textureLoader = new THREE.TextureLoader();
 
 const plantTexture = textureLoader.load('plants.jpg');
@@ -61,7 +73,7 @@ const woodTexture = textureLoader.load('wood.jpg');
 const waterTexture = textureLoader.load('water.jpg');
 const brickTexture = textureLoader.load('brick.jpg');
 
-//objects
+/****************************** OBJECTS ***************************************/
 const boxGeometry = new THREE.BoxGeometry(20, 20, 20);
 const boxMaterial = new THREE.MeshBasicMaterial(
   {
@@ -75,7 +87,7 @@ const box = new THREE.Mesh(boxGeometry, boxMaterial);
 // scene.add(box);
 // box.position.set(-25, 20, 0)
 
-const sphereGeometry = new THREE.SphereGeometry(10, 60, 60);//radius, w, h
+const sphereGeometry = new THREE.SphereGeometry(4, 50, 50);//radius, w, h
 const sphereMaterial = new THREE.MeshStandardMaterial(
   {
     color: 'tomato',
@@ -85,52 +97,89 @@ const sphereMaterial = new THREE.MeshStandardMaterial(
 );
 
 const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
-scene.add(sphere);
-sphere.position.set(-5, 10, 0);
 
-const torusGeometry = new THREE.TorusGeometry(14, 4, 10, 75) ;
+scene.add(sphere);
+sphere.position.set(-10, 0, 0);
+sphere.castShadow = true;
+sphere.receiveShadow = true;
+
+const torusGeometry = new THREE.TorusGeometry(20, 4, 10, 75) ;
 const torusMaterial = new THREE.MeshStandardMaterial(
   {
-    color: 'bisque',
+    color: 'dodgerBlue',
     wireframe: false,
-    normalMap: plantTexture
+    // normalMap: plantTexture
   }
 );
 
 const torus = new THREE.Mesh(torusGeometry, torusMaterial);
-// scene.add(torus);
+scene.add(torus);
 torus.position.set(-5, 10, 0);
+torus.castShadow = true;
+torus.receiveShadow = true;
 
 
-//lights
-const pointLight = new THREE.PointLight(0xF1EB9C);
-pointLight.position.set(-40, 0, 5);
+/****************************** LIGHTS ****************************************/
+// const directionalLight = new THREE.DirectionalLight(0xFFFFFF, 0.8);
+// scene.add(directionalLight);
+// directionalLight.position.set(-30, 50, 0);
+// directionalLight.castShadow = true;
+// directionalLight.shadow.camera.bottom = -12;
+
+// const pointLight = new THREE.PointLight(0xF1EB9C);
+// scene.add(pointLight);
+// pointLight.position.set(-40, 0, 5);
 
 const ambientLight = new THREE.AmbientLight(0x404040);//soft white light
-scene.add(pointLight, ambientLight);
+scene.add(ambientLight);
 
-const lightHelper = new THREE.PointLightHelper(pointLight);
-scene.add(lightHelper);
+const spotLight = new THREE.SpotLight(0xf5f582);
+scene.add(spotLight);
+spotLight.position.set(-200, 100, 0);
+spotLight.castShadow = true;
+spotLight.angle = 0.2 //spotLight shadow is pixelated when angle is too wide
 
-// plane
-const planeGeometry = new THREE.PlaneGeometry(30, 30);
-const planeMaterial = new THREE.MeshBasicMaterial(
+/******************************* PLANE ****************************************/
+const planeGeometry = new THREE.PlaneGeometry(200, 200);
+const planeMaterial = new THREE.MeshStandardMaterial(
   {
-    color: 'chartreuse',
-    transparent: true,
-    opacity: 0.5,
+    color: 0xFFFFFF,
+    // transparent: false,
+    // opacity: 0.5,
     side: THREE.DoubleSide
   }
 );
+
 const plane = new THREE.Mesh(planeGeometry, planeMaterial);
 scene.add(plane);
 plane.rotation.x = -0.5 * Math.PI; //make plane match grid
+plane.receiveShadow = true;
 
+
+/******************************* HELPERS **************************************/
+//grid
 const gridHelper = new THREE.GridHelper(100, 50);
 scene.add(gridHelper);
 
-//animation
+//light helpers
+// const dLightHelper = new THREE.DirectionalLightHelper(directionalLight, 5);
+// scene.add(dLightHelper);
 
+// const pointLightHelper = new THREE.PointLightHelper(pointLight);
+// scene.add(pointLightHelper);
+
+const spotLightHelper = new THREE.SpotLightHelper(spotLight);
+// scene.add(spotLightHelper);
+//camera helpers
+// const dLShadowHelper = new THREE.CameraHelper(directionalLight.shadow.camera)
+// scene.add(dLShadowHelper);
+
+const axesHelper = new THREE.AxesHelper(5);
+// scene.add(axesHelper);
+
+
+
+/****************************** ANIMATION *************************************/
 let step = 0;
 // let speed = 0.01; //move speed of bounce to property in gui options obj
 
@@ -140,15 +189,22 @@ function animate(time) {
   sphere.rotation.x = time / 5000;
   sphere.rotation.y = time / 5000;
   sphere.rotation.z = time / 5000;
-  torus.rotation.x = time / 500;
-  torus.rotation.y = time / 500;
-  torus.rotation.z = time / 2500;
+  torus.rotation.x = time / 5000;
+  torus.rotation.y = time / 5000;
+  torus.rotation.z = time / 3500;
   // pointLight.position.set(Math.random(), 5, 5); //attempt at light flickering
-  pointLight.position.set(-20, 40, 5);
+  // pointLight.position.set(-20, 40, 5);
 
   //bouncing sphere
-  step += options.sphereBounceSpeed
+  step += options.sphereBounceSpeed;
   sphere.position.y = 10 * Math.abs(Math.sin(step));
+
+  //spotLight props
+  spotLight.angle = options.angle;
+  spotLight.penumbra = options.penumbra;
+  spotLight.intensity = options.intensity;
+  spotLight.decay = options.decay;
+  spotLightHelper.update();
 
   orbit.update();
   renderer.render(scene, camera);
